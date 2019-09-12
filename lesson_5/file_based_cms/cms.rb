617b550c5f2335f2374298a52bc8051cfebd21d9
Balal Naeem <balalnaeem@gmail.com>
2019-09-12 10:52:14 +0100

    course completed

---
+++ lesson_5/file_based_cms/cms.rb
@@ -0,0 +1,228 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+require "sinatra/content_for"
+require "tilt/erubis"
+require "redcarpet"
+require "yaml"
+require "bcrypt"
+
+configure do
+  enable :sessions
+  set :session_secret, "secret"
+end
+
+root = File.expand_path("..", __FILE__)
+
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(path)
+  content = File.read(path)
+
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    content
+  when ".md"
+    erb render_markdown(content)
+  end
+end
+
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
+end
+
+def load_user_credentials
+  credentials_path = if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/users.yml", __FILE__)
+  else
+    File.expand_path("../users.yml", __FILE__)
+  end
+
+  YAML.load_file(credentials_path)
+end
+
+def valid_credentials?(username, password)
+  credentials = load_user_credentials
+
+  if credentials.key?(username)
+    bcrypt_password = BCrypt::Password.create(credentials[username])
+    bcrypt_password == password
+  else
+    false
+  end
+end
+
+def user_signed_in?
+  session.key?(:username)
+end
+
+def require_signed_in_user
+  unless user_signed_in?
+    session[:message] = "You must be signed in to do that."
+    redirect "/"
+  end
+end
+
+get "/" do
+  pattern = File.join(data_path, '*')
+  @files_array = Dir.glob(pattern).map do |path|
+    File.basename(path)
+  end
+
+  erb :index
+end
+
+get "/new" do
+  require_signed_in_user
+
+  erb :new
+end
+
+post "/create" do
+  require_signed_in_user
+
+  filename = params[:filename]
+
+  if filename.size == 0
+    session[:message] = "A name is required."
+    status 422
+    erb :new
+  else
+    file_path = File.join(data_path, filename)
+
+    File.write(file_path, "")
+    session[:message] = "#{filename} has been created."
+
+    redirect "/"
+  end
+end
+
+get "/:filename" do
+  filename = params[:filename]
+
+  file_path = File.join(data_path, filename)
+
+  if File.exist?(file_path)
+    load_file_content(file_path)
+  else
+    session[:message] = "#{filename} does not exist."
+    redirect "/"
+  end
+end
+
+get "/:filename/edit" do
+  require_signed_in_user
+
+  file_path = File.join(data_path, params[:filename])
+
+  @filename = params[:filename]
+  @content = File.read(file_path)
+
+  erb :edit
+end
+
+post "/:filename" do
+  require_signed_in_user
+
+  file_path = File.join(data_path, params[:filename])
+
+  File.write(file_path, params[:content])
+
+  session[:message] = "#{params[:filename]} has been updated."
+  redirect "/"
+end
+
+post "/:filename/delete" do
+  require_signed_in_user
+
+  filename = params[:filename]
+  file_path = File.join(data_path, filename)
+
+  session[:message] = "#{filename} has been deleted."
+  File.delete(file_path)
+
+  redirect "/"
+end
+
+get "/users/signin" do
+  erb :signin
+end
+
+post "/users/signin" do
+  username = params[:username]
+
+  if valid_credentials?(username, params[:password])
+    session[:username] = username
+    session[:message] = "Welcome!"
+    redirect "/"
+  else
+    session[:message] = "Invalid credentials"
+    status 422
+    erb :signin
+  end
+end
+
+post "/users/signout" do
+  session.delete(:username)
+  session[:message] = "You have been signed out."
+  redirect "/"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
